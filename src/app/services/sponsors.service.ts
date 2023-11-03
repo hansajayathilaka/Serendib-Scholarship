e908b2c91d7301061679e4d3a0f04bee9838e467
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2023-11-03 11:11:28 +0530

    Remove debugger

--- src/app/services/sponsors.service.ts
+++ src/app/services/sponsors.service.ts
@@ -46,7 +46,6 @@ export class SponsorsService {
                         }
                     }
                 });
-                debugger
                 return sponsors;
             })
         );
